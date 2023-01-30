7cad9e92e834e4d3577975ee40c3e86139e509d6
missvico <vickydiliscia@gmail.com>
2023-01-30 19:24:29 -0300

    resolucion ejercicio promesas

--- 15-promesas.js
+++ 15-promesas.js
@@ -42,12 +42,28 @@ function getCharacter(id){
 
 async function getCharacterAsync(id){
     try{
+        //obtenemos datos del personaje
         const response = await fetch(`https://swapi.dev/api/people/${id}/`)
         const responseBody = await response.json()
         console.log(`El nombre del personaje es ${responseBody.name}`)
+
+        //obtenemos datos del planeta de origen
         const homeworldResponse = await fetch(responseBody.homeworld)
         const homeworldResponseBody = await homeworldResponse.json()
         console.log(`${responseBody.name} es originario de ${homeworldResponseBody.name}`)
+        
+        //obtenemos arreglo de residentes del planeta y lo mapeamos en un arreglo de promesas
+        const promiseArray = homeworldResponseBody.residents.map(function(url){
+             return fetch(url).then(response => response.json())
+        })
+
+        //ejecutamos ese arreglo de promesas utilizando promise all
+        const residents = await Promise.all(promiseArray)
+
+        //trasnformamos el arreglo de objetos a un arreglo de strings que contienen solo el nombre
+        const residentNames = residents.map(resident => resident.name)
+        console.log(`${responseBody.name} es originario de ${homeworldResponseBody.name} al igual que ${residentNames.join(", ")}`)
+
     }catch(error){
         console.log(error)
     }
